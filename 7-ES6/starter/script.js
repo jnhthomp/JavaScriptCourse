dce4db939790617d1e8b334c1d0d6dd82ed26815
Jonah Thompson <jnh.thompson@gmail.com>
2019-10-15 01:59:51 -0700

    Completed 2 lessons in Section 7
    Covered destructuring
    Covered new array functions and new ways to deal with arrays

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -259,6 +259,116 @@
 
 
 
+/*******************************
+ *      109. Destructuring     *
+ *******************************/
+// Convenient way to extract data from a data structure like an object or array
+
+// We have an array filled with data
+//  We want to store each element in that arrray to a variable
+
+//ES5
+// var john5 = ['John', 26];
+//
+// var name5 = john5[0];
+// var age5 = john5[1];
+//
+// // This would be impractical if there were a lot of items in the array
+//
+// //ES6
+// // the first set of brackets sets variables the second set assigns values
+// const [name6, age6] = ['John', 26];
+// console.log(name6, age6);
+//
+// const obj = {
+//   firstName: 'John',
+//   lastName: 'Smith'
+// };
+//
+// const {
+//   firstName,
+//   lastName
+// } = obj;
+// console.log(firstName);
+// console.log(lastName);
+//
+// const {
+//   firstName: a,
+//   lastName: b
+// } = obj;
+// console.log(a);
+// console.log(b);
+
+
+
+// function calcAgeRetirement(year) {
+//   const age = new Date().getFullYear() - year;
+//   return [age, 65 - age];
+// }
+//
+// const [age2, retirement] = calcAgeRetirement(1990);
+//
+// console.log(age2);
+// console.log(retirement);
+
+
+
+/****************************************
+ *      110. Arrays in ES6 / ES2015     *
+ ****************************************/
+
+const boxes = document.querySelectorAll('.box');
+
+//ES5
+boxesArr5 = Array.prototype.slice.call(boxes);
+
+boxesArr5.forEach(function(cur) {
+  cur.style.backgroundColor = 'dodgerblue';
+});
+
+//ES6
+const boxesArr6 = Array.from(boxes);
+boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+//ES5
+// for (var i = 0; i < boxesArr5.length; i++) {
+//   if (boxesArr5[i].className === 'box blue') {
+//     continue;
+//   }
+//
+//   boxesArr5[i].textContent = 'I changed to blue';
+// }
+//
+
+//ES6 forof
+
+for (const cur of boxesArr6) {
+  if (cur.className.includes('blue')) {
+    continue;
+  }
+  cur.textContent = 'I changed to blue';
+}
+
+
+//ES5
+var ages = [12, 17, 8, 21, 14, 11];
+
+var full = ages.map(function(cur) {
+  return cur >= 18;
+});
+
+console.log(full);
+
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
+
+//ES6
+console.log(ages.findIndex(cur => cur >= 18));
+console.log(ages.find(cur => cur >= 18));
+
+
+
+
 
 
 
@@ -288,4 +398,4 @@
 // }
 //
 //
-// generateTitleBlock('108. Arrow Functions: Lexical \'this\' Keyword');
+// generateTitleBlock('110. Arrays in ES6 / ES2015');
