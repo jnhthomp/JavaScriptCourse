0e8f18ff748ff0fc2eaa899fbfd378b40b2a737a
Jonah Thompson <jnh.thompson@gmail.com>
2020-02-02 01:37:16 -0800

    Fixed Timer Rest Bug
    Need to clear the input field when a user input text
    Need to read text before user presses space to collect scores

--- TypingTest/app.js
+++ TypingTest/app.js
@@ -1,49 +1,69 @@
-/******************************
- *      Budget Controller     *
- ******************************/
+/**************************************
+ *      Type Test Data Controller     *
+ **************************************/
 // Controller to handle calculations and stores background data
 // Store list of words to be used
 // Randomizes the order of the words
 //
 // Calculats WPM
 var TypeTestData = (function() {
+  /**********************
+   *      Word List     *
+   **********************/
+  // List of words to be typed
+  // Cannot exceed 10,000
+  var wordList = ['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at'];
 
-  var wordList = ['the',
-    'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at'
-  ];
-
+  /********************************
+   *      Randomize Word List     *
+   ********************************/
+  // Randomize the word list on load/reset button
+  // Uses fisher/yates method https://bost.ocks.org/mike/shuffle/
   var randomizeWordList = function(wordList) {
-    // Uses fisher/yates method https://bost.ocks.org/mike/shuffle/
+    // Sets current index to the end of the array
     var currentIndex = wordList.length,
       temporaryValue, randomIndex;
-
+    // Keep doig this until current index has worked backwards through the array
     while (0 !== currentIndex) {
+      // Gets a random number smaller than the current idex
       randomIndex = Math.floor(Math.random() * currentIndex);
+      // subtract 1 from the current index
       currentIndex -= 1;
-
+      // this variable is given the word that is at the end of the worlist
       temporaryValue = wordList[currentIndex];
+      // Sets the value of the index we have been using to the value of another random word in the array
       wordList[currentIndex] = wordList[randomIndex];
+      // The word that was at the end swaps places with the previously selected word
       wordList[randomIndex] = temporaryValue;
     }
-
+    // After working backwards through every word in the array return the array
     return wordList;
   };
 
-
-
+  /************************
+   *      Score Test      *
+   ************************/
+  // Should be a good way to calculate WPM found here: https://www.speedtypingonline.com/typing-equations
   var scoreTest = function(allTypedEntries, uncorrectedErrors, time) {
+    // Time may need divided by 60 since we are using seconds for our timer
     wpm = ((allTypedEntries / 5) - uncorrectedErrors) / time;
   };
 
+  /******************************
+   *      Public Functions      *
+   ******************************/
   return {
+    // Initiate and setup a new word list
     createWordList: function() {
       return randomizeWordList(wordList);
     },
 
+    // Run calculations to find wpm
     calcWPM: function(allTypedEntries, uncorrectedErrors, time) {
-      return scoreTest();
+      return scoreTest(allTypedEntries, uncorrectedErrors, time);
     },
 
+    // Store any methods used for testing functions in the TypeTestData controller
     testing: {
       randomizeWordList: function() {
         console.log(randomizeWordList(wordList));
@@ -71,17 +91,25 @@ var UIController = (function() {
     timer: 'timer'
   };
 
+  // Formats the array of words to be displayed in UI
   var formatWordList = function(wordList) {
+    // Creates a strign from the array
     wordList = wordList.toString(wordList);
+    // Do this as long as the string contains commas
     while (wordList.includes(',', 0)) {
+      // Take out commas and add spaces 'word1,word2,word3'->'word1 word2 word3'
       wordList = wordList.replace(',', ' ');
     }
+    // Return the formatted word list
     return wordList;
   };
 
+  // Set how long the timer should run for
   var startTime = 10;
+  // Set seconds to the starting time
   var seconds = startTime;
-  var timer;
+  // create a timer variable
+  var timer2;
 
   function countdownTimer() {
     if (seconds <= startTime) {
@@ -90,12 +118,17 @@ var UIController = (function() {
     if (seconds > 0) {
       seconds--;
     } else {
-      clearInterval(timer);
+      clearInterval(timer2);
+      timer2 = false;
       alert('You type x WPM');
     }
   }
 
-
+  var showSeconds = function() {
+    console.log('set seconds to 10');
+    seconds = 10;
+    document.getElementById(domStrings.timer).innerHTML = seconds;
+  };
 
   return {
     updateWordList: function(wordList) {
@@ -116,16 +149,16 @@ var UIController = (function() {
     },
 
     timerStart2: function() {
-      if (!timer) {
-        timer = window.setInterval(function() {
+      console.log(timer2);
+      if (!timer2) {
+        timer2 = window.setInterval(function() {
           UIController.timerStart();
         }, 1000);
       }
     },
 
     resetTimer: function() {
-      console.log('set seconds to 10');
-      seconds = 10;
+      showSeconds();
     }
   };
 })();
@@ -167,7 +200,7 @@ var mainController = (function(TypeTestData, UIController) {
 
 
     document.getElementById(domStrings.typingArea).onkeydown = function() {
-      UIController.timerStart2();
+      UIController.timerStart2(timer);
     };
   };
 
