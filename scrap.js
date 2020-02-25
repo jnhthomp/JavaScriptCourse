3da397f8019a960b0b525b79066d563624ec33d5
jnhthomp <31945842+jnhthomp@users.noreply.github.com>
2020-02-25 01:33:46 -0800

    Deleted

--- scrap.js
+++
@@ -1,9 +0,0 @@
-function genCodes(){
-for(var i = 0; i < 10; i++){
-  randomNum = Math.floor(1000 + Math.random() * 9000);
-  genCode = "50Off" + randomNum;
-  console.log(genCode);
-
-};
-};
-genCodes();
