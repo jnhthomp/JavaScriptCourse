91ecf7bf67d0754a98a36cb95934cff3eb542bd4
Jonah Thompson <jnh.thompson@gmail.com>
2020-02-28 03:33:57 -0800

    Added skeleton for search functionality
    Has limited functionality, just enough to test that Search.js is able to
    return results

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,5 +1,75 @@
-/*jshint esversion: 8*/
 // Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js Variable x is ${x}`);
+
+
+/* Imports */
+import Tools from './tools';
+import Search from './models/Search';
+
+/*  Global state of the app
+|*    -Search object
+|*    -Current recipe object
+|*    -Shopping list object
+|*    -Liked Recipes
+|*/
+const state = {};
+
+
+
+
+const controlSearch = async () => {
+  //  1. Get query from view
+  const query = 'pizza'; // TODO: read data from user from search view module
+
+  //  2. If there is a query then create a new search object and add it to state
+  if (query) {
+    state.search = new Search(query);
+
+    //  3. Prepare UI for results
+
+    //  4. Search for recipes
+    await state.search.getResults();
+
+    //  5. Render results on UI
+    console.log(state.search.result);
+  }
+};
+
+
+document.querySelector('.search').addEventListener('submit', (e) => {
+  e.preventDefault();
+  controlSearch();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Show time on reload
+function showTime() {
+  const tools = new Tools();
+  const time = tools.logTime();
+  console.log(`Reloaded -- ${time}`);
+}
+showTime();
