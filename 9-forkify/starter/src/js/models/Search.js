eca1809b42a9bdfb15fb13aa41441e0587485dbf
Jonah Thompson <jnh.thompson@gmail.com>
2020-02-28 20:38:40 -0800

    Added steps outling Search class
    Described steps for getResults()

--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -3,15 +3,24 @@
 import axios from 'axios';
 //  Export defines what this js file will send to other js files that import it
 export default class Search {
+  //  1.  Accept a query
+  //  Save it as a parameter of an object so we can return an object
   constructor(query) {
     this.query = query;
   }
+
+  //  2. Retrieve the results from the API
   async getResults() {
-    try {
+    //  Use try/catch for error handling
+    try { // Do this stuff
+      // axios is used because it is compatible with old browsers
+      // axios also automatically converts to json
       const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+      //  3. Isolate the list of recipes that were returned by the APIs and save it to this object to be accessed by index.js
+      //  Save the recipes from the above returned object to results property of this object (currently in index.js state.Search) so state.Search will have access to the now created state.Search.result
       this.result = res.data.recipes;
       //console.log(this.result);
-    } catch (error) {
+    } catch (error) { // Do this stuff if the above stuff fails
       alert(error);
     }
   }
