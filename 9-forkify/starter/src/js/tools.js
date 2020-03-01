2f980d156c9108694df9c3ff8964dbd23e0736ab
Jonah Thompson <jnh.thompson@gmail.com>
2020-02-29 18:46:42 -0800

    Updated to now be useable in index.js

--- 9-forkify/starter/src/js/tools.js
+++ 9-forkify/starter/src/js/tools.js
@@ -1,26 +1,17 @@
-export default class Tools {
+export const logTime = () => {
+  const today = new Date();
+  const hours = ('0' + today.getHours()).slice(-2);
+  const minutes = ('0' + today.getMinutes()).slice(-2);
+  const seconds = ('0' + today.getSeconds()).slice(-2);
 
-  constructor(lessonNum, title) {
-    this.lessNum = lessonNum;
-    this.title = title;
-  }
+  const time = `${hours}:${minutes}:${seconds}`;
+  return time;
+};
 
-  // Get current time. Useful for seeing when the last time the auto webdev-dev-server reloaded was
-  logTime() {
-    const today = new Date();
-    const hours = ('0' + today.getHours()).slice(-2);
-    const minutes = ('0' + today.getMinutes()).slice(-2);
-    const seconds = ('0' + today.getSeconds()).slice(-2);
-
-    const time = `${hours}:${minutes}:${seconds}`;
-    return time;
-  }
-
-  titleBlock(lessonNum, title) {
-    const mRow = `\ *      ${lessonNum}. ${title}      *`;
-    const tRow = `/${'*'.repeat(mRow.length - 1)}`;
-    const bRow = `\ ${'*'.repeat(mRow.length -1)}/`;
-    const titleBlock = `${tRow}\n${mRow}\n${bRow}`;
-    return titleBlock;
-  }
-}
+export const titleBlock = (lessonNum, title) => {
+  const mRow = `\ *      ${lessonNum}. ${title}      *`;
+  const tRow = `/${'*'.repeat(mRow.length - 1)}`;
+  const bRow = `\ ${'*'.repeat(mRow.length -1)}/`;
+  const titleBlock = `${tRow}\n${mRow}\n${bRow}`;
+  return titleBlock;
+};
