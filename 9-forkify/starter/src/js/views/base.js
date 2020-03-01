33a2e2bcf6f57f66730c6d4f48d5d6e4b50541e6
Jonah Thompson <jnh.thompson@gmail.com>
2020-02-29 20:28:48 -0800

    Added renderLoader() and clearLoader()
    Creates and deletes loading icons

--- 9-forkify/starter/src/js/views/base.js
+++ 9-forkify/starter/src/js/views/base.js
@@ -1,9 +1,38 @@
+// Used to hold DOM strings
 export const elements = {
   searchForm: document.querySelector('.search'),
   searchInput: document.querySelector('.search__field'),
+  searchRes: document.querySelector('.results'),
   searchResList: document.querySelector('.results__list')
 };
 
-export const renderLoader = (parent) => {
+// Used to hold DOM strings without the period for use inside of html
+// Doesn't contain document.querySelector because there are instances we need to use the string only and without querySelector
+export const elementStrings = {
+  loader: 'loader' // class of loading icon
+};
 
+// Render loading icon within a specified parent class
+export const renderLoader = (parent) => {
+  // Loader css to insert
+  const loader = (
+    `<div class="${elementStrings.loader}">
+      <svg>
+        <use href="img/icons.svg#icon-cw"></use>
+      <svg>
+     </div>`
+  );
+
+  // Insert the html into the parent that was specified at the top
+  parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+// Delete a rendered loader from the screen
+export const clearLoader = () => {
+  // specify the loader. There should only be one at a time so we shouldn't need to specify a parent
+  const loader = document.querySelector(`.${elementStrings.loader}`);
+  if (loader) { // If the loader exists
+    // Delete the loader icon
+    loader.parentElement.removeChild(loader);
+  }
 };
